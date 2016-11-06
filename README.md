e83d13fe97f44ab1500db39673fc3be3a117f6e9
Starfox64 <louisdijon21@yahoo.fr>
2016-11-06 21:02:54 +0100

    Added wheels and finished install.py (Needs more testing)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pygame-installer
 A small Python script to install the pygame library.
+
+# Credits
+- [Christoph Gohlke][wheels] (Python Wheels)
+
+[wheels]: http://www.lfd.uci.edu/~gohlke/pythonlibs/#pygame
---
+++ install.py
@@ -0,0 +1,57 @@
+import platform, sys
+
+try:
+	import urllib.request as urllib_request
+except ImportError:
+	import urllib2 as urllib_request
+
+def httpGet(url):
+	response = urllib_request.urlopen(url)
+	return response.read().decode('utf-8')
+
+hasPip = False
+
+try:
+	import pip
+	hasPip = True
+except ImportError:
+	pass
+
+if not hasPip:
+	getPip = httpGet("https://bootstrap.pypa.io/get-pip.py")
+	exec(getPip)
+	import pip
+
+arch = platform.architecture()[0]
+wheelUrl = "https://raw.githubusercontent.com/Starfox64/pygame-installer/master/wheels/"
+
+print("You are using Python" + str(sys.version_info[0]) + str(sys.version_info[1]) + " " + arch)
+
+if sys.version_info[0] == 2 and sys.version_info[1] == 7:
+	if arch == "64bit":
+		wheelUrl += "pygame-1.9.2b1-cp27-cp27m-win_amd64.whl"
+	else:
+		wheelUrl += "pygame-1.9.2b1-cp27-cp27m-win32.whl"
+elif sys.version_info[0] == 3 and sys.version_info[1] in (4, 5, 6):
+	if sys.version_info[1] == 4:
+		if arch == "64bit":
+			wheelUrl += "pygame-1.9.2b1-cp34-cp34m-win_amd64.whl"
+		else:
+			wheelUrl += "pygame-1.9.2b1-cp34-cp34m-win32.whl"
+	elif sys.version_info[1] == 5:
+		if arch == "64bit":
+			wheelUrl += "pygame-1.9.2b1-cp35-cp35m-win_amd64.whl"
+		else:
+			wheelUrl += "pygame-1.9.2b1-cp35-cp35m-win32.whl"
+	elif sys.version_info[1] == 6:
+		if arch == "64bit":
+			wheelUrl += "pygame-1.9.2b8-cp36-cp36m-win_amd64.whl"
+		else:
+			wheelUrl += "pygame-1.9.2b8-cp36-cp36m-win32.whl"
+else:
+	print("Pygame only supports Python 27, 34, 35 and 36.")
+	sys.exit()
+
+pip.main(["install", wheelUrl])
+
+print("If nothing red showed up, Pygame should be installed now.")
